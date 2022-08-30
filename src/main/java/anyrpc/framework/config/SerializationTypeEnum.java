53e655760091d6a169d60065a172c673a5f4cec9
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-08-30 21:01:01 +0800

    feat:添加序列化相关contents

---
+++ src/main/java/anyrpc/framework/config/SerializationTypeEnum.java
@@ -0,0 +1,35 @@
+package anyrpc.framework.config;
+
+import com.sun.xml.internal.ws.developer.Serialization;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+/**
+ * @author fzw
+ * 序列化相关的枚举
+ * @date 2022-08-30 19:08
+ */
+
+@Getter
+@AllArgsConstructor
+public enum SerializationTypeEnum {
+    //kyro
+    KYRO((byte)0x01,"kyro"),
+    PROTOSTUFF((byte)0x02,"protostuff"),
+    HESSIAN((byte)0x03,"hessian");
+
+    private final byte code;
+
+    private final String name;
+
+    public static String getName(byte code) {
+        for (SerializationTypeEnum c : SerializationTypeEnum.values()) {
+            if (c.getCode() == code) {
+                return c.name;
+            }
+        }
+        return null;
+    }
+
+}
