8561fa0fc460878026047218d42c8207777b9b6b
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-08-30 16:02:12 +0800

    feat:添加rpc相关消息题的响应码

---
+++ src/main/java/anyrpc/framework/config/RpcCodeEnum.java
@@ -0,0 +1,21 @@
+package anyrpc.framework.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+/**
+ * @author fzw
+ * rpc相关响应码
+ * @date 2022-08-30 15:58
+ */
+@Getter
+@AllArgsConstructor
+public enum  RpcCodeEnum {
+    /** 响应码 */
+    SUCCESS(200, "成功"),
+    FAIL(500, "失败");
+
+    private final int code;
+    private final String message;
+}
