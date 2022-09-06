d9e3cc82e91285971aa98b0a332f9faee4200aa9
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-09-06 17:24:44 +0800

    feat:添加zk注册中心

---
+++ src/main/java/anyrpc/framework/registry/zookeeper/ZkServiceRegistry.java
@@ -0,0 +1,20 @@
+package anyrpc.framework.registry.zookeeper;
+
+import anyrpc.framework.registry.ServiceRegistry;
+import org.apache.curator.framework.CuratorFramework;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author fzw
+ * zookeeper的服务注册
+ * @date 2022-09-01 10:26
+ */
+public class ZkServiceRegistry implements ServiceRegistry {
+    @Override
+    public void registerService(String serviceName, InetSocketAddress inetSocketAddress) {
+        String servicepath = CuratorUtils.ZK_REGISTER_ROOT_PATH + "/" + serviceName + inetSocketAddress.toString();
+        CuratorFramework zkClient = CuratorUtils.getZkClient();
+        CuratorUtils.createPersistentNode(zkClient, servicepath);
+    }
+}
