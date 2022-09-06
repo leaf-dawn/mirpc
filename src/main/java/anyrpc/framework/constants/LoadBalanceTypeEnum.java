5a7fb74840ba8b72bf7b6c2f4fffb0e0a2324c59
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-09-06 16:04:34 +0800

    feat:添加负载均衡类型

---
+++ src/main/java/anyrpc/framework/constants/LoadBalanceTypeEnum.java
@@ -0,0 +1,32 @@
+package anyrpc.framework.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author fzw
+ * 负载均衡选择
+ * @date 2022-09-06 15:58
+ */
+@Getter
+@AllArgsConstructor
+public enum LoadBalanceTypeEnum {
+
+    //random
+    RANDOM((byte)0x01,"random"),
+    CONSISTENT_HASH((byte)0x02,"consistentHash");
+
+    private final byte code;
+
+    private final String name;
+
+    public static String getName(byte code) {
+        for (LoadBalanceTypeEnum c : LoadBalanceTypeEnum.values()) {
+            if (c.getCode() == code) {
+                return c.name;
+            }
+        }
+        return null;
+    }
+
+}
