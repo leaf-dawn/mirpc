59d269df687959f89da580870ab8e306884d5331
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-09-11 16:38:26 +0800

    feat:添加runtime相关工具包

---
+++ src/main/java/anyrpc/framework/utils/RuntimeUtil.java
@@ -0,0 +1,17 @@
+package anyrpc.framework.utils;
+
+/**
+ * @author fzw
+ * runtime相关工具包
+ * @date 2022-09-11 16:36
+ */
+public class RuntimeUtil {
+
+    /**
+     * 获取cpu核数
+     * @return :cpu核数
+     */
+    public static int cpus() {
+        return Runtime.getRuntime().availableProcessors();
+    }
+}
