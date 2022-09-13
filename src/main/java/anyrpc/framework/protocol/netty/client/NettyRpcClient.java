e16fed638f7af938eecf362c1d22970e55af3027
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-09-13 21:42:35 +0800

    feat:添加channelProvider对channel进行管理

---
+++ src/main/java/anyrpc/framework/protocol/netty/client/NettyRpcClient.java
@@ -0,0 +1,11 @@
+package anyrpc.framework.protocol.netty.client;
+
+/**
+ * @author fzw
+ *  https://blog.csdn.net/weixin_31080716/article/details/113089494
+ *  是否要实现断开重连
+ * rpc客户端
+ * @date 2022-09-13 19:50
+ */
+public class NettyRpcClient {
+}
