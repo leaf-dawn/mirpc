75b49ed3cd3bba1ca8bb4405f3c55977dc8861a3
fzw-code <81961566+fzw-code@users.noreply.github.com>
2022-09-20 21:44:26 +0800

    feat:添加rpcScan注解

---
+++ src/main/java/anyrpc/framework/annotation/RpcScan.java
@@ -0,0 +1,17 @@
+package anyrpc.framework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author nbzw
+ * @date 2022-09-19 18:04
+ */
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface RpcScan {
+}
